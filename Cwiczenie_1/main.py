ac946c2974d9cbde734ea4520d2ed814e9b8ba5c
Łukasz Szpak <alukasz1@interia.pl>
2021-03-09 23:58:29 +0100

    Don't ask

--- Cwiczenie_1/main.py
+++ Cwiczenie_1/main.py
@@ -6,24 +6,22 @@ from Cwiczenie_1.dataLoading import DataLoading
 COUNTER = 100000
 
 if __name__ == '__main__':
-    data_load = DataLoading("lab1_problemy_testowe/zad2.txt")
+    data_load = DataLoading("lab1_problemy_testowe/zad0.txt")
     data = data_load.get_data()
     pcb = PCB(data[1], data[0][0], data[0][1])
 
-    best_score = 9999
-    best_solution = None
+    # best_score = 9999
+    # best_solution = None
     # for i in range(COUNTER):
     #     if pcb.make_random_solution():
     #         temp_score = pcb.calculate_and_get_quality()
     #         if temp_score < best_score:
     #             best_score = temp_score
     #             best_solution = copy.deepcopy(pcb)
+    # print(best_solution.matrix)
+    # print(best_score)
 
     while not pcb.make_random_solution():
-        temp_score = pcb.calculate_and_get_quality()
-        if temp_score < best_score:
-            best_score = temp_score
-            best_solution = copy.deepcopy(pcb)
-
-    print(best_solution.matrix)
-    print(best_score)
+        pcb.calculate_and_get_quality()
+    print(pcb.matrix)
+    print(pcb.calculate_and_get_quality())
