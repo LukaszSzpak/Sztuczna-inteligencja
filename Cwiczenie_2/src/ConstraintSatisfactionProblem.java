7ae8011bac6d98116b246d12d7a35be479ea6f40
Łukasz Szpak <alukasz1@interia.pl>
2021-03-30 14:00:29 +0200

    Create project, added CSP and backTracking algorithm

---
+++ Cwiczenie_2/src/ConstraintSatisfactionProblem.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ConstraintSatisfactionProblem<V, D> {
+    private List<V> variables;
+    private Map<V, List<D>> domains;
+    private Map<V, List<Constraint<V, D>>> constraints;
+
+    public ConstraintSatisfactionProblem(List<V> variables, Map<V, List<D>> domains) {
+        this.variables = variables;
+        this.domains = domains;
+        constraints = new HashMap<>();
+        for (V variable : variables) {
+            constraints.put(variable, new ArrayList<>());
+            if (!domains.containsKey(variable)) {
+                throw new IllegalArgumentException("Every variable should have a domain assigned to it.");
+            }
+        }
+    }
+
+    public void addConstraint(Constraint<V, D> constraint) {
+        for (V variable : constraint.variables) {
+            if (!variables.contains(variable)) {
+                throw new IllegalArgumentException("Variable in constraint not in CSP");
+            } else {
+                constraints.get(variable).add(constraint);
+            }
+        }
+    }
+
+    public boolean consistent(V variable, Map<V, D> assignment) {
+        for (Constraint<V, D> constraint : constraints.get(variable)) {
+            if (!constraint.satisfied(assignment)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private Map<V, D> backTrackingSearch(Map<V, D> assignment) {
+        if (assignment.size() == variables.size()) {
+            return assignment;
+        }
+
+        V unassigned = variables.stream().filter(v -> (!assignment.containsKey(v))).findFirst().get();
+        for (D value : domains.get(unassigned)) {
+            Map<V, D> localAssignment = new HashMap<>(assignment);
+            localAssignment.put(unassigned, value);
+
+            if (consistent(unassigned, localAssignment)) {
+                Map<V, D> result = backTrackingSearch(localAssignment);
+                if (result != null) {
+                    return result;
+                }
+            }
+        }
+        return null;
+    }
+
+    public Map<V, D> backTrackingSearch() {
+        return backTrackingSearch(new HashMap<>());
+    }
+}
