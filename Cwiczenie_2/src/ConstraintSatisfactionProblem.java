77c820d09f9c81e5eb5eabeb2cf72f43858aff88
Łukasz Szpak <alukasz1@interia.pl>
2021-04-06 18:16:01 +0200

    Added heuristics for next var choice

--- Cwiczenie_2/src/ConstraintSatisfactionProblem.java
+++ Cwiczenie_2/src/ConstraintSatisfactionProblem.java
@@ -1,9 +1,12 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class ConstraintSatisfactionProblem<V, D> {
     private final List<V> variables;
     private final Map<V, List<D>> domains;
     private final Map<V, List<Constraint<V, D>>> constraints;
+    private final static String HEURISTIC = "first"; //[first, mostEdges]
+
 
     public ConstraintSatisfactionProblem(List<V> variables, Map<V, List<D>> domains) {
         this.variables = variables;
@@ -41,7 +44,7 @@ public class ConstraintSatisfactionProblem<V, D> {
             return assignment;
         }
 
-        V unassigned = variables.stream().filter(v -> (!assignment.containsKey(v))).findFirst().get();
+        V unassigned = getNextUnassignedVariable(assignment);
         for (D value : domains.get(unassigned)) {
             Map<V, D> localAssignment = new HashMap<>(assignment);
             localAssignment.put(unassigned, value);
@@ -54,6 +57,33 @@ public class ConstraintSatisfactionProblem<V, D> {
             }
         }
         return null;
+    }
+
+    private V getNextUnassignedVariable(Map<V, D> assignment) {
+        switch (ConstraintSatisfactionProblem.HEURISTIC) {
+            case "first": return getFirstVariable(assignment);
+            case "mostEdges": return getMostEdgesVariable(assignment);
+            default: throw new IllegalArgumentException("Wrong heuristic choice!");
+        }
+    }
+
+    private V getFirstVariable(Map<V, D> assignment) {
+        return variables.stream().filter(v -> (!assignment.containsKey(v))).findFirst().get();
+    }
+
+    private V getMostEdgesVariable(Map<V, D> assignment) {
+        List<V> variablesWithoutAssigment = variables.stream().filter(v ->
+                (!assignment.containsKey(v))).collect(Collectors.toList());
+        int most = 0;
+        V bestVariable = variablesWithoutAssigment.get(0);
+        for (V variable : variablesWithoutAssigment) {
+            if (this.constraints.get(variable).size() > most) {
+                most = this.constraints.get(variable).size();
+                bestVariable = variable;
+            }
+        }
+
+        return bestVariable;
     }
 
     public Map<V, D> backTrackingSearch() {
