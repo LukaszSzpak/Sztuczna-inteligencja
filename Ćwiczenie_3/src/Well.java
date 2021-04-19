b4b4911ee5c8a8a43f2ac5ee02e9f0adbaca67fd
Łukasz Szpak <alukasz1@interia.pl>
2021-04-19 20:56:05 +0200

    Implemented well

--- Ćwiczenie_3/src/Well.java
+++ Ćwiczenie_3/src/Well.java
@@ -1,3 +1,15 @@
 public class Well {
+    private int stonesCounter;
 
+    public Well() {
+        this.stonesCounter = 0;
+    }
+
+    public void addStone() {
+        this.stonesCounter++;
+    }
+
+    public int getScore() {
+        return this.stonesCounter;
+    }
 }
