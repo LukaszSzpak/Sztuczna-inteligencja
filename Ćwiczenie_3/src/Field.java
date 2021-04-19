662e1d72945ea4ee800704f2f6fdccf4e984a55e
Łukasz Szpak <alukasz1@interia.pl>
2021-04-19 20:58:54 +0200

    Implemented field

--- Ćwiczenie_3/src/Field.java
+++ Ćwiczenie_3/src/Field.java
@@ -1,2 +1,21 @@
 public class Field {
+    private int stonesCounter;
+
+    public Field() {
+        this.stonesCounter = 0;
+    }
+
+    public void addStone() {
+        this.stonesCounter++;
+    }
+
+    public int getNumberOfStones() {
+        return stonesCounter;
+    }
+
+    public int getAndEraseStones() {
+        int temp = this.stonesCounter;
+        this.stonesCounter = 0;
+        return temp;
+    }
 }
