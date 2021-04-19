79d612f5348afd771b1a0039c78f1d40acb0c0c9
Łukasz Szpak <alukasz1@interia.pl>
2021-04-19 20:56:47 +0200

    Implemented player interface

--- Ćwiczenie_3/src/Player.java
+++ Ćwiczenie_3/src/Player.java
@@ -1,2 +1,8 @@
 public interface Player {
+    static final int NUMBER_OF_STONES = 4;
+    static final int NUMBER_OF_FIELDS = 6;
+
+    void initializePlayer();
+    int move();
+    int getWellScore();
 }
